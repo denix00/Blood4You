bed0f45a1c8c5e0b85a8914aff549e1b0a2d1497
denix00 <denix00@gmail.com>
2015-04-26 01:23:37 +0200

    dodana WhyDonateBlood aktivnost i clickHandler za nju

--- app/src/main/java/tvz/trip/tvzmc2/blood4you/WhyDonateBlood.java
+++ app/src/main/java/tvz/trip/tvzmc2/blood4you/WhyDonateBlood.java
@@ -1,25 +1,23 @@
 package tvz.trip.tvzmc2.blood4you;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class WhyDonateBlood extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_why_donate_blood);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_why_donate_blood, menu);
         return true;
     }
 
@@ -37,12 +35,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void clickHandlerWhyDonate(View view)
-    {
-        Intent intentWhyDonate = new Intent(this, WhyDonateBlood.class);
-        startActivity(intentWhyDonate);
-    }
-
-
 }
