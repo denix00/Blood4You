fc901d4786b68825174f9073df94079af532a720
denix00 <denix00@gmail.com>
2015-04-29 02:44:17 +0200

    mergano

---
+++ app/src/main/java/tvz/trip/tvzmc2/blood4you/HelpDialogClass.java
@@ -0,0 +1,30 @@
+package tvz.trip.tvzmc2.blood4you;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+
+
+public class HelpDialogClass extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.help_dialog, null))
+
+                .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        HelpDialogClass.this.getDialog().cancel();
+                    }
+                });
+
+        return builder.create();
+    }
+
+}
